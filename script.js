d66dcec07341198da2b8fdfeb81b0af3b7e9e55f
stealthman1 <timur.marufiy@gmail.com>
2022-08-11 16:17:11 +0300

    some bugfixing

--- script.js
+++ script.js
@@ -137,11 +137,7 @@ let fullscreen = false;
 
 // Toggle Fullscreen
 function toggleFullscreen() {
-    if (!fullscreen) {
-        openFullscreen(player);
-    } else {
-        closeFullscreen();
-    }
+    !fullscreen ? openFullscreen(player) :  closeFullscreen();
     fullscreen = !fullscreen;
 }
 
